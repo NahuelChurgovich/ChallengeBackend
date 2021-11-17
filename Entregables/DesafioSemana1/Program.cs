41d7db6a44d43429306e06c11a9c24dcd028e6a1
NahuelChurgovich <nchurgovich@gmail.com>
2021-11-17 20:27:30 -0300

    First commit

---
+++ Entregables/DesafioSemana1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Desafio1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
